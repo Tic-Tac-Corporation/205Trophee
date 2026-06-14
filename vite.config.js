1d07e5f9f2d21b2f2d45d189a928447ec7804126
JRobert <jeremy100401@gmail.com>
2026-06-14 20:08:02 +0200

    correction vite-config pour gh-pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/205Trophee/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
